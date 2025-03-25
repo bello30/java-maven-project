49f8f66fa2d09ad106cf7ed5d32e7c2cb5c36533
Bello Opeyemi <bello30353@gmail.com>
2025-03-25 08:18:47 +0100

    Resolving merge conflict

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,7 @@ FROM openjdk:8-jre-alpine
 
 EXPOSE 8080
 
-<<<<<<< HEAD
 COPY ./target/java-maven-app-1.0-SNAPSHOT.jar /usr/app/
 WORKDIR /usr/app
 
 ENTRYPOINT ["java", "-jar", "java-maven-app-1.0-SNAPSHOT.jar"]
-=======
-COPY ./target/java-maven-app-1.1.0-SNAPSHOT.jar /usr/app/
-WORKDIR /usr/app
-
-ENTRYPOINT ["java", "-jar", "java-maven-app-1.1.0-SNAPSHOT.jar"]
->>>>>>> 4a076336e09569c25b250ebf5bae8951e7337f30
--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 package com.example;
 
-=======
-
-package com.example;
->>>>>>> 4a076336e09569c25b250ebf5bae8951e7337f30
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
@@ -13,7 +8,6 @@ import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class Application {
-<<<<<<< HEAD
 
     public static void main(String[] args)
     {
@@ -30,22 +24,4 @@ public class Application {
     public String getStatus() {
         return "OK";
     }
-=======
-	
-	public static void main(String[] args)
-	{
-		SpringApplication.run(Application.class, args);
-	}
-	
-	@PostConstruct
-	public void init()
-	{
-		Logger log = LoggerFactory.getLogger(Application.class);
-		log.info("Java app started");
-	}
-	
-	public String getStatus() {
-		return "OK";
-	}
->>>>>>> 4a076336e09569c25b250ebf5bae8951e7337f30
 }
